16944149c23dfe53cb08f821881a48af2a4e6b26
Krathegon <fabian@familie-kalkofen.de>
2019-07-21 21:12:55 +0200

    added serial import for windows

--- Software/CWSuperExtremeHeavyDigitalWaterfall3000Brain/CwSerial.py
+++ Software/CWSuperExtremeHeavyDigitalWaterfall3000Brain/CwSerial.py
@@ -1,11 +1,15 @@
 from serial import Serial
+import platform
 
 
 class CwSerial():
 
     def __init__(self):
         # self.serial_port = None
-        self.serial_port = Serial('/dev/ttyUSB0', 115200)
+        if platform.system() == 'Linux':
+            self.serial_port = Serial('/dev/ttyUSB0', 115200)
+        else:
+            self.serial_port = Serial('COM3', 115200)
 
     def write(self, bytes):
         self.serial_port.write(bytes)
