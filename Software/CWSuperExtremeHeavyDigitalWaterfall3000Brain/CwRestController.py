8efd2f97998425f4252a34611b353516837bb7d0
Krathegon <fabian@familie-kalkofen.de>
2019-07-22 22:26:13 +0200

    adjusted some effects to be more bold edited the rest controller

--- Software/CWSuperExtremeHeavyDigitalWaterfall3000Brain/CwRestController.py
+++ Software/CWSuperExtremeHeavyDigitalWaterfall3000Brain/CwRestController.py
@@ -1,3 +1,5 @@
+import string
+
 from flask import Flask, Response, request
 
 from Settings import Settings
@@ -7,7 +9,11 @@ class CwRestController():
 
     def __init__(self):
         self.app = FlaskAppWrapper('wrap')
-        self.app.add_endpoint(endpoint='/set_timing', endpoint_name='set_timing', handler=self.action)
+        self.app.add_endpoint(endpoint='/settings', endpoint_name='settings',
+                              endpoint_action=EndpointAction(self.action))
+        self.available_effects_endpoint_action = EndpointAction(self.available_effects)
+        self.app.add_endpoint(endpoint='/available_effects', endpoint_name='available_effects',
+                              endpoint_action=self.available_effects_endpoint_action)
 
     def run(self):
         self.app.run()
@@ -15,6 +21,15 @@ class CwRestController():
     def action(self):
         Settings.on_time = float(request.args.get('on_time'))
         Settings.off_time = float(request.args.get('off_time'))
+        Settings.effect_name = request.args.get('effect_name')
+        # Settings.height = float(request.args.get('height'))
+
+    def available_effects(self):
+        self.available_effects_endpoint_action.response = Response(
+            response=string.join(Settings.available_effects, ";"),
+            status=200,
+            mimetype="text/txt",
+            headers={})
 
 
 class EndpointAction(object):
@@ -28,6 +43,12 @@ class EndpointAction(object):
         return self.response
 
 
+class EndpointActionWithResponse(EndpointAction):
+
+    def __call__(self, *args):
+        return self.action()
+
+
 class FlaskAppWrapper(object):
     app = None
 
@@ -35,7 +56,10 @@ class FlaskAppWrapper(object):
         self.app = Flask(name)
 
     def run(self):
-        self.app.run()
+        self.app.run(threaded=True)
 
-    def add_endpoint(self, endpoint=None, endpoint_name=None, handler=None):
-        self.app.add_url_rule(endpoint, endpoint_name, EndpointAction(handler))
+    # def add_endpoint(self, endpoint=None, endpoint_name=None, handler=None):
+    #    self.app.add_url_rule(endpoint, endpoint_name, EndpointAction(handler))
+
+    def add_endpoint(self, endpoint=None, endpoint_name=None, endpoint_action=None):
+        self.app.add_url_rule(endpoint, endpoint_name, endpoint_action)
--- Software/CWSuperExtremeHeavyDigitalWaterfall3000Brain/Settings.py
+++ Software/CWSuperExtremeHeavyDigitalWaterfall3000Brain/Settings.py
@@ -1,8 +1,19 @@
+import os
+
+
 class Settings():
     height = 1.0
     repeats = 100000
-    on_time = 0.00001
+    on_time = 0.03
+    #    on_time = 0.001
     off_time = 0.0
     on_time_diff = 0.01
     off_time_pause = 1.0
-    effect_name = "one"
+    effect_name = "three"
+    available_effects = []
+
+
+for r, d, files in os.walk("."):
+    for name in files:
+        if name.endswith('.txt'):
+            Settings.available_effects.append(name[:-4])
